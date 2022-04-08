9c5d65729d06c7f5cc8d48bc0df26b093019144c
Alyce <Alyceosbourne@outlook.com>
2022-04-08 21:51:21 +0100

    requirements

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # algorithm to create a dict of notes and frequencies
 # and then create a sine wave
 import time
-from enum import Enum, auto
-from math import pi, log2
-from functools import partial, cache
+from enum import Enum
+from functools import cache, partial
 
 import numpy as np
 from pyaudio import paInt32, PyAudio
+import keyboard
 
 FORMAT = paInt32
 CHANNELS = 1
@@ -45,20 +45,23 @@ class Note(Enum):
     @staticmethod
     def _sine(frequency, duration, sample_rate):
         samples = int(sample_rate * duration)
-        sine_wave = np.sin(2 * pi * frequency * np.arange(samples) / sample_rate)
+        sine_wave = np.sin(2 * np.pi * frequency * np.arange(samples) / sample_rate)
         return sine_wave.astype(np.float32)
 
     @cache
     def sine(self, octave, duration, sample_rate):
         return self._sine(self.frequency(octave), duration, sample_rate)
 
+    # square wave
     @staticmethod
     def _square(frequency, duration, sample_rate):
         samples = int(sample_rate * duration)
-        sine_wave = np.sin(2 * pi * frequency * np.arange(samples) / sample_rate)
-        square_wave = sine_wave.astype(np.float32)
-        square_wave[square_wave > 0] = 1
-        square_wave[square_wave < 0] = -1
+        square_wave = np.zeros(samples, dtype=np.float32)
+        for i in range(samples):
+            if i % (sample_rate // frequency) < sample_rate // frequency / 2:
+                square_wave[i] = 1
+            else:
+                square_wave[i] = -1
         return square_wave
 
     @cache
@@ -66,110 +69,139 @@ class Note(Enum):
         return self._square(self.frequency(octave), duration, sample_rate)
 
     @staticmethod
+    def _triangle_wave(frequency, duration, sample_rate):
+        # output must be between -1 and 1
+        samples = int(sample_rate * duration)
+        triangle_wave = np.zeros(samples, dtype=np.float32)
+        for i in range(samples):
+            if i % (sample_rate // frequency) < sample_rate // frequency / 2:
+                triangle_wave[i] = 2 * (i % (sample_rate // frequency) / (sample_rate // frequency)) - 1
+            else:
+                triangle_wave[i] = 2 * (1 - (i % (sample_rate // frequency) / (sample_rate // frequency))) - 1
+
+        # scale to -1 to 1
+        return triangle_wave * 2 + 1
+
+    @cache
+    def triangle(self, octave, duration, sample_rate):
+        return self._triangle_wave(self.frequency(octave), duration, sample_rate)
+
+    # sawtooth wave
+    @staticmethod
     def _sawtooth(frequency, duration, sample_rate):
         samples = int(sample_rate * duration)
-        sine_wave = np.sin(2 * pi * frequency * np.arange(samples) / sample_rate)
-        sawtooth_wave = sine_wave.astype(np.float32)
-        sawtooth_wave[sawtooth_wave > 0] = 1
-        sawtooth_wave[sawtooth_wave < 0] = -1
+        sawtooth_wave = np.zeros(samples, dtype=np.float32)
+        for i in range(samples):
+            sawtooth_wave[i] = 2 * (i % (sample_rate // frequency) / (sample_rate // frequency)) - 1
         return sawtooth_wave
 
     @cache
     def sawtooth(self, octave, duration, sample_rate):
         return self._sawtooth(self.frequency(octave), duration, sample_rate)
 
+    @cache
+    def wave(self, octave, wave_function, duration, sample_rate):
+        return getattr(self, wave_function)(octave, duration, sample_rate)
+
     @staticmethod
-    def _triangle(frequency, duration, sample_rate):
-        samples = int(sample_rate * duration)
-        sine_wave = np.sin(2 * pi * frequency * np.arange(samples) / sample_rate)
-        triangle_wave = sine_wave.astype(np.float32)
-        triangle_wave[triangle_wave > 0] = 1
-        triangle_wave[triangle_wave < 0] = -1
-        triangle_wave = triangle_wave * 2 - 1
-        return triangle_wave
+    def _oscillator(note, octave, wave_function, duration, sample_rate):
+        while True:
+            yield getattr(note, wave_function)(octave, duration, sample_rate)
+
+    def oscillator(self, wave_function, octave, duration, sample_rate):
+        yield from self._oscillator(self, octave, wave_function, duration, sample_rate)
+
+    @staticmethod
+    def _oscillators(note, octave, wave_functions, duration, sample_rate):
+        return [getattr(note, wave_function)(octave, duration, sample_rate) for wave_function in wave_functions]
+
+    def oscillators(self, wave_functions, octave, duration, sample_rate):
+        return self._oscillators(self, octave, wave_functions, duration, sample_rate)
+
+    @staticmethod
+    def mix_samples(samples):
+        return np.sum(samples, axis=0)
+
+
+class AudioStream:
+    def __init__(self, data_format=FORMAT, channels=CHANNELS, rate=RATE, chunk=CHUNK):
+        self.format = data_format
+        self.channels = channels
+        self.rate = rate
+        self.chunk = chunk
+
+    @staticmethod
+    @cache
+    def pyaudio_context():
+        return PyAudio()
 
     @cache
-    def triangle(self, octave, duration, sample_rate):
-        return self._triangle(self.frequency(octave), duration, sample_rate)
+    def pyaudio_stream(self):
+        return self.pyaudio_context().open(format=self.format,
+                                           channels=self.channels,
+                                           rate=self.rate,
+                                           output=True,
+                                           frames_per_buffer=self.chunk)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.pyaudio_stream().stop_stream()
+        self.pyaudio_stream().close()
+        self.pyaudio_stream.cache_clear()
+
+    def play(self, samples):
+        self.pyaudio_stream().write(samples)
 
 
-@cache
-def pyaudio_context():
-    return PyAudio()
+# a synthesizer that plays notes from octave 0 to octave 10 for every note sorted by frequency when keys are pressed
+class Synthesizer:
+    def __init__(self):
+        self.keys = {}
+        for octave in range(0, 11):
+            for note in Note:
+                self.keys[f"{note.name}{octave}"] = partial(note.wave, octave)
+
+    def play(self, key, wave, duration, sample_rate):
+        with AudioStream() as audio_stream:
+            audio_stream.play(self.keys[key](wave, duration, sample_rate))
 
 
-@cache
-def pyaudio_stream():
-    return pyaudio_context().open(format=FORMAT,
-                                  channels=CHANNELS,
-                                  rate=RATE,
-                                  output=True,
-                                  frames_per_buffer=CHUNK)
+    # use the keyboard module to map keys to notes and then create a synthesizer that plays the notes
+    def keyboard_synthesizer(self):
+        while True:
+            if keyboard.is_pressed('1'):
+                self.play('C4', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('2'):
+                self.play('D4', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('3'):
+                self.play('E4', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('4'):
+                self.play('F4', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('5'):
+                self.play('G4', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('6'):
+                self.play('A4', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('7'):
+                self.play('B4', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('8'):
+                self.play('C5', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('9'):
+                self.play('D5', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('0'):
+                self.play('E5', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('q'):
+                self.play('C4', 'sine', 0.5, 44100)
+            if keyboard.is_pressed('w'):
+                self.play('D4', 'sine', 0.5, 44100)
 
 
-def close_stream():
-    pyaudio_stream().close()
-    pyaudio_stream.cache_clear()
+
+if __name__ == "__main__":
+   Synthesizer().keyboard_synthesizer()
 
 
-def play_note(note, octave, duration, sample_rate=RATE, wave_type="sine"):
-    stream = pyaudio_stream()
-    stream.write(getattr(note, wave_type)(octave, duration, sample_rate))
 
 
-def play_chord(notes: list[tuple[Note, int]], duration, sample_rate=RATE, wave_type="sine"):
-    # add two waves together using numpy
-    stream = pyaudio_stream()
-    chord = None
-    for note, octave in notes:
-        if chord is None:
-            chord = getattr(note, wave_type)(octave, duration, sample_rate)
-        else:
-            chord += getattr(note, wave_type)(octave, duration, sample_rate)
 
-    stream.write(chord.tobytes())
-
-
-def play_sequence(notes, octaves, durations, sample_rate=RATE, wave_type="sine"):
-    stream = pyaudio_stream()
-    for note, octave, duration in zip(notes, octaves, durations):
-        stream.write(getattr(note, wave_type)(octave, duration, sample_rate))
-        time.sleep(duration)
-
-
-def play_chord_sequence(notes: list[list[tuple[Note, int]]], durations, delay, sample_rate=RATE, wave_type="sine"):
-    for notes, duration in zip(notes, durations):
-        play_chord(notes, duration, sample_rate, wave_type)
-        time.sleep(delay)
-
-
-def play_song(song, sample_rate=RATE, wave_type="sine"):
-    for note, octave, duration in song:
-        play_note(note, octave, duration, sample_rate, wave_type)
-
-
-song = (
-    (Note.C, 4, 0.5),
-    (Note.D, 4, 0.5),
-    (Note.E, 4, 0.5),
-    (Note.F, 4, 0.5),
-    (Note.G, 4, 0.5),
-    (Note.A, 4, 0.5),
-    (Note.B, 4, 0.5),
-    (Note.C, 5, 0.5),
-
-)
-
-play_song(song, wave_type="triangle")
-
-play_chord_sequence(
-    notes=[
-    [(Note.C, 4), (Note.F, 4), (Note.G, 4)],
-    [(Note.C, 4), (Note.A, 4), (Note.D_SHARP, 4)],
-    [(Note.C, 4), (Note.D, 4), (Note.E, 4)],
-    [(Note.C, 4), (Note.D, 4), (Note.F, 4)],
-    [(Note.C, 4), (Note.D, 4), (Note.G, 4)]
-],
-    durations=[0.5, 0.5, 0.5, 0.5, 0.5],
-    delay=0.5,
-    wave_type="sawtooth")
