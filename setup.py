a35ec08ad1d3a4100e38312f9ce69f87945abf85
Alyce <Alyceosbourne@outlook.com>
2022-04-08 02:06:34 +0100

    requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='synthesizer',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Alyce',
+    author_email='',
+    description=''
+)
